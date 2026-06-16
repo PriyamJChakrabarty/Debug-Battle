43dbaed234a5d20eb3d56cb17beea7ff275ae719
PriyamJChakrabarty <144861619+PriyamJChakrabarty@users.noreply.github.com>
2026-06-17 00:05:03 +0530

    Full Mechanics I
    First version of Full Mechanics

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
-import DebugBattleClient from "./debug-battle-client";
-import { getDebugBattlePayload } from "@/lib/debug-battle";
+import { readFileSync } from "fs";
+import { join } from "path";
+import VulnerabilityClient from "./vulnerability-client";
 
-export default async function Home() {
-  const initialPayload = await getDebugBattlePayload();
-  return <DebugBattleClient initialPayload={initialPayload} />;
+export default function Home() {
+  const dataPath = join(process.cwd(), "public", "data", "data.json");
+  const data = JSON.parse(readFileSync(dataPath, "utf-8"));
+
+  // data.Code is relative to public/data/ (e.g. "../codes/UserManager.cpp")
+  const codePath = join(process.cwd(), "public", "data", data.Code);
+  const code = readFileSync(codePath, "utf-8");
+
+  return <VulnerabilityClient data={data} code={code} />;
 }
